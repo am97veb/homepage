0a7db3fd23f2d1ac851dd53d2d07bdc2b8c271ea
Adrian Markefka <am97veb@gmail.com>
2025-06-18 12:34:59 +0200

    Button operation improved - now the button changes the colour of all section headers and the background of the header, article and footer

--- js/script.js
+++ js/script.js
@@ -1,29 +1,34 @@
 {
-    const welcome = () => {
-     console.log("witam");
-    };
-
     const toggleButton = () => {
         const body = document.querySelector(".js-body");
-        const sectionHeader = document.querySelector(".js-section__header");
+        const header = document.querySelector(".js-header");
+        const sectionHeader = document.querySelectorAll(".js-section__header");
+        const sectionArticle = document.querySelectorAll(".js-section__article");
         const tableCaption = document.querySelector(".js-table__caption");
+        const buttonName = document.querySelector(".js-button__icon");
         const footerHeader = document.querySelector(".js-footer__header");
-        const buttonName = document.querySelector(".js-button__name");
+        const footerContact = document.querySelector(".js-footer__contact");
 
         body.classList.toggle("body--light");
-        sectionHeader.classList.toggle("section__header--dark");
         tableCaption.classList.toggle("table__caption--dark");
         footerHeader.classList.toggle("footer__header--dark");
+        header.classList.toggle("header--light");
+        footerContact.classList.toggle("footer__contact--light");
 
-        buttonName.innerText = body.classList.contains("body--light") ? "ciemne" : "jasny";
+        sectionHeader.forEach(header => {
+            header.classList.toggle("section__header--dark");
+        });
+
+        sectionArticle.forEach(article => {
+            article.classList.toggle("section__article--light");
+        });
+
+        buttonName.innerText = body.classList.contains("body--light") ? "🌙" : "🌞";
     };
 
     const init = () => {
         const button = document.querySelector(".js-button");
         button.addEventListener("click", toggleButton);
-
-        welcome();
     };
-        init();
+    init();
 };
-    
